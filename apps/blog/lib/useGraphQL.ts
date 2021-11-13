10d1ff1995c7b6e5d74181420ab25687cd7333da
Douglas Moura <douglas.ademoura@gmail.com>
2021-11-13 10:22:26 -0300

    feat: lista os posts na home

---
+++ apps/blog/lib/useGraphQL.ts
@@ -0,0 +1,35 @@
+import { useEffect, useState } from 'react'
+
+async function fetchGraphQL<Data>(text, variables) {
+  return fetch(`http://localhost:4000/graphql`, {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify({
+      query: text,
+      variables,
+    }),
+  })
+  .then(res => res.json() as Promise<{ data: Data }>)
+  .then(res => res.data)
+}
+
+export function useGraphQL<Data>(query: string, variables?: { [key: string]: string | number }) {
+  const [error, setError] = useState<Error | null>(null)
+  const [loading, setLoading] = useState(false)
+  const [data, setData] = useState<Data>()
+
+  useEffect(() => {
+    if (!query) return
+
+    setLoading(true)
+
+    fetchGraphQL<Data>(query, variables)
+      .then((data) => setData(data))
+      .catch((error) => setError(error))
+      .finally(() => setLoading(false))
+  }, [query, variables])
+
+  return { error, loading, data }
+}
--- apps/blog/pages/_app.tsx
+++ apps/blog/pages/_app.tsx
@@ -1,6 +1,7 @@
 import { AppProps } from 'next/app';
 import Head from 'next/head';
-import './styles.css';
+
+
 
 function CustomApp({ Component, pageProps }: AppProps) {
   return (
@@ -9,14 +10,7 @@ function CustomApp({ Component, pageProps }: AppProps) {
         <title>Welcome to blog!</title>
       </Head>
       <div className="app">
-        <header className="flex">
-          {/* eslint-disable-next-line @next/next/no-img-element */}
-          <img src="/nx-logo-white.svg" alt="Nx logo" width="75" height="50" />
-          <h1>Welcome to blog!</h1>
-        </header>
-        <main>
-          <Component {...pageProps} />
-        </main>
+        <Component {...pageProps} />
       </div>
     </>
   );
